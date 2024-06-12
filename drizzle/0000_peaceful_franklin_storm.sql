9631bdeb949d6c94c7b05bbf8201019884e18f72
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-06-12 15:16:59 -0300

    `npm install typeBox` é config migration

---
+++ drizzle/0000_peaceful_franklin_storm.sql
@@ -0,0 +1,61 @@
+DO $$ BEGIN
+ CREATE TYPE "public"."status" AS ENUM('Available', 'Maintenance', 'Loaned', 'Reserved');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "author" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"firstName" varchar(100) NOT NULL,
+	"familyName" varchar(100) NOT NULL,
+	"dateOfBirth" date,
+	"dateOfDeath" date,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"uptadedAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "genre" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"uptadedAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "book" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"isbn" varchar(17) NOT NULL,
+	"summary" text NOT NULL,
+	"authorId" integer,
+	"genreId" integer,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"uptadedAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "bookInstance" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"bookId" integer,
+	"imprint" text NOT NULL,
+	"status" "status" DEFAULT 'Maintenance' NOT NULL,
+	"dueBack" date DEFAULT now(),
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"uptadedAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "book" ADD CONSTRAINT "book_authorId_author_id_fk" FOREIGN KEY ("authorId") REFERENCES "public"."author"("id") ON DELETE set null ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "book" ADD CONSTRAINT "book_genreId_genre_id_fk" FOREIGN KEY ("genreId") REFERENCES "public"."genre"("id") ON DELETE set null ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "bookInstance" ADD CONSTRAINT "bookInstance_bookId_book_id_fk" FOREIGN KEY ("bookId") REFERENCES "public"."book"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
