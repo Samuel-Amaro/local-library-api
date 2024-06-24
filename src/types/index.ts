415afbe8e95e71502e45fcbba933cb696fc78057
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-06-24 14:01:49 -0300

    adiciona `types`

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export enum Order {
+  asc = "asc",
+  desc = "desc",
+}
+
+export enum Status {
+  available = "available",
+  maintenance = "maintenance",
+  loaned = "loaned",
+  reserved = "reserved",
+}
